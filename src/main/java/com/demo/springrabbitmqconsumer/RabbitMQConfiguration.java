b4080cc91f0789e722c815f33fc0661aba7aa488
Dan Gitau <DMGITAU@safaricom.co.ke>
2022-07-23 15:45:12 +0300

    Task queue implementation:- set lower prefetch, and lower threshold to trigger concurrency. Concurrency set 1-20

--- src/main/java/com/demo/springrabbitmqconsumer/RabbitMQConfiguration.java
+++ src/main/java/com/demo/springrabbitmqconsumer/RabbitMQConfiguration.java
@@ -1,10 +1,13 @@
 package com.demo.springrabbitmqconsumer;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,9 @@ public class RabbitMQConfiguration {
     final static String QUEUE_NAME = "statements_message_queue";
     final static String EXCHANGE   = "statements_message_exchange";
     final static String ROUTING_KEY  = "statements_message_routing_key";
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     @Bean
     public Queue queue ()
@@ -29,7 +35,7 @@ public class RabbitMQConfiguration {
     public Binding binding (Queue queue, TopicExchange exchange)
     {
         return BindingBuilder
-                .bind(queue).to(exchange)   //direct binding
+                .bind(queue).to(exchange)
                 .with(ROUTING_KEY);
     }
     @Bean
@@ -38,12 +44,17 @@ public class RabbitMQConfiguration {
         // convert to json
         return new Jackson2JsonMessageConverter();
     }
-    @Bean
-    public AmqpTemplate template (ConnectionFactory connectionFactory)
-    {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        return template;
-    }
 
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        factory.setConcurrentConsumers(1);
+        factory.setMaxConcurrentConsumers(20);
+        factory.setConsecutiveActiveTrigger(1);
+        factory.setConsecutiveIdleTrigger(1);
+        factory.setPrefetchCount(10);
+        return factory;
+    }
 }
--- src/main/java/com/demo/springrabbitmqconsumer/RabbitMQMessageConsumer.java
+++ src/main/java/com/demo/springrabbitmqconsumer/RabbitMQMessageConsumer.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQMessageConsumer {
-
-    @RabbitListener (queues = RabbitMQConfiguration.QUEUE_NAME)
-    public  void consumerListener (Message message)
-    {
+    //@RabbitListener (queues = RabbitMQConfiguration.QUEUE_NAME)   //THIS IS DEFAULT BEAN IMPLEMEMNTATIN THAT'S SEQUENTIAL IN NATURE
+    @RabbitListener (queues = RabbitMQConfiguration.QUEUE_NAME, containerFactory = "rabbitListenerContainerFactory")   //THIS IS THREADED - Executor service
+    public void consumerListener(Message message) throws InterruptedException {
+        //Thread.sleep(1000);
         System.out.println(message);
     }
 }
